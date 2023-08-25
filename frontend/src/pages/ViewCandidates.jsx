47e3560a651a22c85a75151c712e3a8568ff0a94
Prakash A <prakashaathi321@gmail.com>
2023-08-25 21:39:45 +0530

    init view candidates

---
+++ frontend/src/pages/ViewCandidates.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const ViewCandidates = () => {
+    return (
+        <div className='flex h-screen'>
+            <Navbar />
+            ViewCandidates
+        </div>
+    )
+}
+
+export default ViewCandidates
