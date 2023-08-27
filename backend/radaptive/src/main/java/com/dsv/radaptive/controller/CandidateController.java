0e1a5279df79f4071fb56d4e09ad6c053c27127d
Prakash A <prakashaathi321@gmail.com>
2023-08-27 19:35:15 +0530

    BE added edit candidate endpoint

--- backend/radaptive/src/main/java/com/dsv/radaptive/controller/CandidateController.java
+++ backend/radaptive/src/main/java/com/dsv/radaptive/controller/CandidateController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +46,11 @@ public class CandidateController {
     @GetMapping("/{id}")
     public ResponseEntity<CandidateModel> getCandidateById(@PathVariable Long id) {
         return candidateService.getCandidateById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CandidateModel> updateCandidate(@PathVariable Long id, @Valid @RequestBody CandidateModel candidateModel) {
+        return candidateService.updateCandidate(id, candidateModel);
     }
     
 
--- backend/radaptive/src/main/java/com/dsv/radaptive/service/CandidateService.java
+++ backend/radaptive/src/main/java/com/dsv/radaptive/service/CandidateService.java
@@ -2,6 +2,7 @@ package com.dsv.radaptive.service;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.Valid;
 
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.dsv.radaptive.exception.CandidateNotFoundException;
 import com.dsv.radaptive.model.CandidateModel;
 import com.dsv.radaptive.repository.CandidateRepo;
 
@@ -41,7 +43,40 @@ public class CandidateService {
         return ResponseEntity.ok(candidateRepo.findById(id).get());
     }
 
-    
+    public ResponseEntity<CandidateModel> updateCandidate(Long id, @Valid CandidateModel candidateModel) {
+        Optional<CandidateModel> optionalCandidate = candidateRepo.findById(id);
 
-    
+        if (optionalCandidate.isPresent()) {
+            CandidateModel existingCandidate = optionalCandidate.get();
+
+            if (!existingCandidate.equals(candidateModel)) {
+                existingCandidate.setEmail(candidateModel.getEmail());
+                existingCandidate.setRecoveryEmail(candidateModel.getRecoveryEmail());
+                existingCandidate.setFirstName(candidateModel.getFirstName());
+                existingCandidate.setLastName(candidateModel.getLastName());
+                existingCandidate.setTitle(candidateModel.getTitle());
+                existingCandidate.setCandidateGender(candidateModel.getCandidateGender());
+                existingCandidate.setDob(candidateModel.getDob());
+                existingCandidate.setDescription(candidateModel.getDescription());
+                existingCandidate.setMobileNumber(candidateModel.getMobileNumber());
+                existingCandidate.setAddress1(candidateModel.getAddress1());
+                existingCandidate.setAddress2(candidateModel.getAddress2());
+                existingCandidate.setCity(candidateModel.getCity());
+                existingCandidate.setPostalCode(candidateModel.getPostalCode());
+                existingCandidate.setCandidateCountry(candidateModel.getCandidateCountry());
+                existingCandidate.setHomeLocationCity(candidateModel.getHomeLocationCity());
+                existingCandidate.setCandidateState(candidateModel.getCandidateState());
+
+                CandidateModel updatedCandidate = candidateRepo.save(existingCandidate);
+
+                return ResponseEntity.ok(updatedCandidate);
+            } else {
+                return ResponseEntity.ok(existingCandidate);
+            }
+        } else {
+            throw new CandidateNotFoundException(id);
+        }
+
+    }
+
 }
